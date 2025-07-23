9b4d2821601d195453b1c2ded32f56eea56dffee
Matteo Checcacci <matteocheccacci@gmail.com>
2025-07-23 12:40:26 +0200

    added service

---
+++ downloads.kekkotech.com/project-list.js
@@ -0,0 +1,14 @@
+const projectList = [
+  {
+    "name": "Kekkotech Default Site",
+    "directoryName": "defaultktech",
+    "icon": "https://kekkotech.com/img/logo.png",
+    "zip": "404",
+    "info": "https://downloads.kekkotech.com/projects/defaultktech/defaultktech.html",
+    "authors": "Matteo Checcacci",
+    "version": "0.1",
+    "size": "0.0",
+    "description": "Sito web a linea di comando.",
+    "video": "404"
+  }
+];
