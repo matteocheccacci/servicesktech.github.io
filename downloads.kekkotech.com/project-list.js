699ce037ac84ba9f23e412f8d3456e997e4da876
Matteo Checcacci <matteocheccacci@gmail.com>
2025-07-26 12:28:43 +0200

    list update

--- downloads.kekkotech.com/project-list.js
+++ downloads.kekkotech.com/project-list.js
@@ -10,5 +10,17 @@ const projectList = [
     "size": "0.0",
     "description": "Sito web a linea di comando.",
     "video": "404"
+  },
+  {
+    "name": "Pr ova",
+    "directoryName": "prova",
+    "icon": "404",
+    "zip": "404",
+    "info": "https://downloads.kekkotech.com/projects/prova/prova.html",
+    "authors": "services.kekkotech.com",
+    "version": "0.0.0",
+    "size": "0.0",
+    "description": "Prova",
+    "video": "404"
   }
 ];
